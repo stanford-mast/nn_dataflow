cb6a20b15d52c0c1db4fd80eababbe82c8279119
Mingyu Gao <mgao12@stanford.edu>
2017-03-16 14:39:02 -0700

    [PhyDim2] Fix bug.

--- nn_dataflow/PhyDim2.py
+++ nn_dataflow/PhyDim2.py
@@ -38,19 +38,19 @@ class PhyDim2(namedtuple('PhyDim2', ['h', 'w'])):
         ''' Return element-wise `self + other`. '''
         if not isinstance(other, PhyDim2):
             other = PhyDim2(other, other)
-        return PhyDim2(map(add, self, other))
+        return PhyDim2(*map(add, self, other))
 
     def __sub__(self, other):
         ''' Return element-wise `self - other`. '''
         if not isinstance(other, PhyDim2):
             other = PhyDim2(other, other)
-        return PhyDim2(map(sub, self, other))
+        return PhyDim2(*map(sub, self, other))
 
     def __mul__(self, other):
         ''' Return element-wise `self * other`. '''
         if not isinstance(other, PhyDim2):
             other = PhyDim2(other, other)
-        return PhyDim2(map(mul, self, other))
+        return PhyDim2(*map(mul, self, other))
 
     __rmul__ = __mul__
 
